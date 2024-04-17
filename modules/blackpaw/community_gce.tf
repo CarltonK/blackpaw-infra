55f290e2a4447de6c51a72ba97c407207fc8a4f0
Mark Carlton <mcarlton33@gmail.com>
2024-04-17 22:30:30 +0300

    init vm

--- modules/blackpaw/community_gce.tf
+++ modules/blackpaw/community_gce.tf
@@ -1,13 +1,13 @@
-# module "blackpaw" {
-#   source     = "../compute_instance"
-#   project_id = var.project_id
-#   name       = "blackpaw-community"
-#   zone_id    = var.zone_id
-#   instance_tags = [
-#     "blackpaw",
-#     "odoo",
-#     "community"
-#   ]
+module "blackpaw" {
+  source     = "../compute_instance"
+  project_id = var.project_id
+  name       = "blackpaw-community"
+  zone_id    = var.zone_id
+  instance_tags = [
+    "blackpaw",
+    "odoo",
+    "community"
+  ]
 
-#   depends_on = [ google_project_service.blackpaw-services ]
-# }
+  depends_on = [ google_project_service.blackpaw-services ]
+}
