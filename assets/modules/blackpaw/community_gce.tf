2eb1e3ee3a1707f03825ae242c073e2b2e4817de
Mark Carlton <mcarlton33@gmail.com>
2024-04-17 22:48:04 +0300

    set higher machine type

--- assets/modules/blackpaw/community_gce.tf
+++ assets/modules/blackpaw/community_gce.tf
@@ -1,13 +1,14 @@
-module "blackpaw" {
-  source     = "../compute_instance"
-  project_id = var.project_id
-  name       = "blackpaw-community"
-  zone_id    = var.zone_id
+module "compute_instance" {
+  source       = "../compute_instance"
+  project_id   = var.project_id
+  name         = "blackpaw-community"
+  machine_type = "n1-standard-1"
+  zone_id      = var.zone_id
   instance_tags = [
     "blackpaw",
     "odoo",
     "community"
   ]
 
-  depends_on = [ google_project_service.blackpaw-services ]
+  depends_on = [google_project_service.blackpaw-services]
 }
