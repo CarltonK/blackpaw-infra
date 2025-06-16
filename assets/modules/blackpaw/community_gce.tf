5727252b423b0cafc6b68b525d4b75d41f23239f
Mark Carlton <mcarlton33@gmail.com>
2025-06-16 19:31:21 +0300

    create a new VM

--- assets/modules/blackpaw/community_gce.tf
+++ assets/modules/blackpaw/community_gce.tf
@@ -1,14 +1,14 @@
-# module "compute_instance" {
-#   source       = "../compute_instance"
-#   project_id   = var.project_id
-#   name         = "blackpaw-community"
-#   machine_type = "e2-standard-2"
-#   instance_tags = [
-#     "blackpaw",
-#     "odoo",
-#     "community"
-#   ]
-# #   startup_script = file("../../assets/scripts/odoo_setup.sh")
+module "compute_instance" {
+  source       = "../compute_instance"
+  project_id   = var.project_id
+  name         = "blackpaw-community"
+  machine_type = "e2-standard-2"
+  instance_tags = [
+    "blackpaw",
+    "odoo",
+    "community"
+  ]
+#   startup_script = file("../../assets/scripts/odoo_setup.sh")
 
-#   depends_on = [google_project_service.blackpaw-services]
-# }
+  depends_on = [google_project_service.blackpaw-services]
+}
