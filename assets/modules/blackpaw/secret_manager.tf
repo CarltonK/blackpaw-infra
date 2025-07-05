e3b246134ea2e110d4ced7e5dec2441381b9b216
Mark Carlton <mcarlton33@gmail.com>
2025-07-05 18:02:14 +0300

    Create mpesa secret

--- assets/modules/blackpaw/secret_manager.tf
+++ assets/modules/blackpaw/secret_manager.tf
@@ -9,3 +9,15 @@ resource "google_secret_manager_secret" "contabo_secret" {
     }
   }
 }
+
+resource "google_secret_manager_secret" "mpesa_secret" {
+  secret_id = "mpesa"
+  project   = var.project_id
+  replication {
+    user_managed {
+      replicas {
+        location = var.region
+      }
+    }
+  }
+}
